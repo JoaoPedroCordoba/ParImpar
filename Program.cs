a92949b6618b481fcfa0d4283e6bea50634aa432
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-04-01 20:33:14 -0300

    finalizadoexercicio

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int  n;
+
+Console.WriteLine("Digite um numero:");
+n = Convert.ToInt32(Console.ReadLine());
+
+bool par = (n % 2 ) == 0;
+
+if  (par)
+{ 
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{n} é um numero par");
+} 
+
+else{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"{n} é um numero impar");
+}
+Console.ResetColor();
